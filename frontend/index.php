f7f05e1bd3c8e6dcdebad37a6ee13a39107a6fd2
DoThanhLoc2334 <locdothanh233@gmail.com>
2025-07-21 13:23:51 +0700

    add footer, header for frontend

---
+++ frontend/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shop DTL</title>
+    <?php include_once(__DIR__ . '/../assets/layouts/styles.php'); ?>
+</head>
+<body>
+    
+    <?php include_once(__DIR__ . '/../assets/layouts/scripts.php'); ?>
+
+</body>
+</html>
